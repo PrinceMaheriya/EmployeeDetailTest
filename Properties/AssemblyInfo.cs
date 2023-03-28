1dbdab4d9a641792eb6af00b403fa3ceb23427a0
PrinceMaheriya <1995princejain@gmail.com>
2023-03-29 03:39:27 +0530

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeDetailTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeDetailTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("466ee25f-0265-4335-8eb2-6bf037a69864")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeeDetailTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllEmployeeData()
+        {
+            var result = WpfApp1.Helper.EmployeeService.GetEmployeeData();
+        }
+
+        [TestMethod]
+        public void GetEmployeeDetails()
+        {
+            var employeeId = 1;
+            var result = WpfApp1.Helper.EmployeeService.GetEmployeeDetails(employeeId);
+        }
+
+        [TestMethod]
+        public void AddNewEmployee()
+        {
+
+            WpfApp1.Datum emplDetail = new WpfApp1.Datum();
+            emplDetail.Name = "abc";
+            emplDetail.Email = "abc@gmail.com";
+            emplDetail.Status = WpfApp1.Helper.Common.inactive;
+            emplDetail.Gender = WpfApp1.Helper.Common.male;
+
+            var response = WpfApp1.Helper.EmployeeService.AddEmployee(emplDetail);
+
+        }
+
+        [TestMethod]
+        public void UpdateEmployeeDetails()
+        {
+
+            WpfApp1.Datum emplDetail = new WpfApp1.Datum();
+            emplDetail.Id = 1;
+            emplDetail.Name = "abc";
+            emplDetail.Email = "abc@gmail.com";
+            emplDetail.Status = WpfApp1.Helper.Common.inactive;
+            emplDetail.Gender = WpfApp1.Helper.Common.male;
+
+            WpfApp1.Helper.EmployeeService.UpdateEmployeeDetailsAsync(emplDetail);
+
+        }
+
+        [TestMethod]
+        public void DeleteEmployeeData()
+        {
+           var employeeId = 1;
+           WpfApp1.Helper.EmployeeService.DeleteEmployeeData(employeeId);
+        }
+
+        [TestMethod]
+        public void ValidateEmail()
+        {
+            var emailId = "abc@gmail.com";
+            WpfApp1.Helper.Common.IsValidEmail(emailId);
+        }
+
+
+        [TestMethod]
+        public void NumericText()
+        {
+            var txt = "abc@gmail.com";
+            WpfApp1.Helper.Common.IsTextAllowed(txt);
+        }
+
+    }
+}
