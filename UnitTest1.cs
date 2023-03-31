21899736f2d7ecac8be7cbe54635d121f5c5169e
PrinceMaheriya <1995princejain@gmail.com>
2023-03-31 12:22:11 +0530

    added more test cases

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,7 +9,10 @@ namespace EmployeeDetailTest
         [TestMethod]
         public void GetAllEmployeeData()
         {
-            var result = WpfApp1.Helper.EmployeeService.GetEmployeeData();
+            var pageNo = 1;
+            var result = WpfApp1.Helper.EmployeeService.GetEmployeeData(pageNo);
+
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
@@ -17,57 +20,88 @@ namespace EmployeeDetailTest
         {
             var employeeId = 1;
             var result = WpfApp1.Helper.EmployeeService.GetEmployeeDetails(employeeId);
+
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public void AddNewEmployee()
         {
+            try
+            {
+                WpfApp1.Datum emplDetail = new WpfApp1.Datum();
+                emplDetail.Name = "abc";
+                emplDetail.Email = "abc@gmail.com";
+                emplDetail.Status = WpfApp1.Helper.Common.inactive;
+                emplDetail.Gender = WpfApp1.Helper.Common.male;
+                
+                var response = WpfApp1.Helper.EmployeeService.AddEmployee(emplDetail);
 
-            WpfApp1.Datum emplDetail = new WpfApp1.Datum();
-            emplDetail.Name = "abc";
-            emplDetail.Email = "abc@gmail.com";
-            emplDetail.Status = WpfApp1.Helper.Common.inactive;
-            emplDetail.Gender = WpfApp1.Helper.Common.male;
-
-            var response = WpfApp1.Helper.EmployeeService.AddEmployee(emplDetail);
+                Assert.AreSame(response.IsSuccessStatusCode, true);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
 
         }
 
         [TestMethod]
         public void UpdateEmployeeDetails()
         {
+            try
+            {
 
-            WpfApp1.Datum emplDetail = new WpfApp1.Datum();
-            emplDetail.Id = 1;
-            emplDetail.Name = "abc";
-            emplDetail.Email = "abc@gmail.com";
-            emplDetail.Status = WpfApp1.Helper.Common.inactive;
-            emplDetail.Gender = WpfApp1.Helper.Common.male;
+                WpfApp1.Datum emplDetail = new WpfApp1.Datum();
+                emplDetail.Id = 1;
+                emplDetail.Name = "abc";
+                emplDetail.Email = "abc@gmail.com";
+                emplDetail.Status = WpfApp1.Helper.Common.inactive;
+                emplDetail.Gender = WpfApp1.Helper.Common.male;
 
-            WpfApp1.Helper.EmployeeService.UpdateEmployeeDetailsAsync(emplDetail);
+                WpfApp1.Helper.EmployeeService.UpdateEmployeeDetailsAsync(emplDetail);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
 
         }
 
         [TestMethod]
         public void DeleteEmployeeData()
         {
-            var employeeId = 1;
-            WpfApp1.Helper.EmployeeService.DeleteEmployeeData(employeeId);
+            try
+            {
+                var employeeId = 1;
+                WpfApp1.Helper.EmployeeService.DeleteEmployeeData(employeeId);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
         }
 
         [TestMethod]
         public void ValidateEmail()
         {
             var emailId = "abc@gmail.com";
-            WpfApp1.Helper.Common.IsValidEmail(emailId);
+            var expected = true;
+            var result = WpfApp1.Helper.Common.IsValidEmail(emailId);
+            Assert.AreEqual(expected, result);
         }
 
 
         [TestMethod]
         public void NumericText()
         {
-            var txt = "abc@gmail.com";
-            WpfApp1.Helper.Common.IsTextAllowed(txt);
+           var txt = "abc@gmail.com";
+            var expected = false;
+
+           var result = WpfApp1.Helper.Common.IsTextAllowed(txt);
+
+           Assert.AreEqual(expected, result);
         }
 
     }
